bb2a8a39ef5f1047752b7dd6a8237c867c234270
chingweich <cat8007@gmail.com>
2014-12-09 00:34:56 +0800

    Create submit.sh

---
+++ submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rWindow=4.5
+for (( i=1; i<22; i=i+1 ))
+do
+in=$i
+infilename="root://grid71.phy.ncu.edu.tw//dpm/phy.ncu.edu.tw/home/cms/store/user/chchen/DYToMuMu_M-20_TuneZ2star_14TeV-pythia6-tauola/crab_DY_14TeV/141106_184757/0000/DY_$in.root"
+
+#echo  "in=$in"
+#echo  "infilename=$infilename"
+#./runJob.csh $PWD $infilename $rWindow
+bsub -R "type=SLC6_64" -q 1nd $PWD/runJob.csh $PWD $infilename $rWindow
