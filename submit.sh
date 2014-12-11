4e2083b671324f21a3348655a3fb755f700babc3
chingweich <cat8007@gmail.com>
2014-12-12 00:56:22 +0800

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -9,13 +9,13 @@ do
 done < $file
 for (( i=1; i<=${#lines[@]}; i=i+1 ))
 do
-
+newDir="$i"of"$rWindow"
 
 
 #echo  "in=$in"
 #echo  "infilename=$infilename"
 #./runJob.csh $PWD $infilename $rWindow
-bsub -R "type=SLC6_64" -q 1nd $PWD/runJob.csh $PWD $ ${lines[$i]} $rWindow $i 
+bsub -R "type=SLC6_64" -q 1nd $PWD/runJob.csh $PWD $ ${lines[$i]} $rWindow newDir="$i"of"$rWindow"
 done
 
 
